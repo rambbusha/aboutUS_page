3d3ca8f30549d9502df0885173beba280248e84f
rambbusha <chaitalideore@gmail.com>
2018-07-25 11:57:06 +0200

    Added Separate CSS and JS file for Video Section

---
+++ js/video_section_script.js
@@ -0,0 +1,55 @@
+ 
+            $( document ).ready(function(){ 
+
+
+                $( "#play_button" ).click(function() {
+
+                    $('#play_button_image').hide(); 
+                    $( "#parent iframe" ).attr('src', 'https://www.youtube.com/embed/K3vOSmkhCOM?rel=0&amp;amp;controls=1;autoplay=1');
+                   
+
+                });
+
+                $( "#rami_video" ).click(function() {
+
+                    $('#play_button_image').hide(); 
+                    $( "#parent iframe" ).attr('src', 'https://www.youtube.com/embed/K3vOSmkhCOM?rel=0&amp;amp;controls=1;autoplay=1');
+                     $('.testimonial-text').text("It is better to travel well than to arrive-Rami");
+                });
+
+                $( "#andrea_video" ).click(function() {
+
+                    $('#play_button_image').hide(); 
+                    $( "#parent iframe" ).attr('src','https://www.youtube.com/embed/SR0XcQR9lOU?rel=0&amp;amp;controls=1;autoplay=1');
+                     $('.testimonial-text').text("It is better to travel well than to arrive- Andrea");
+                });
+
+                $( "#jovita_video" ).click(function() {
+
+                    $('#play_button_image').hide(); 
+                    $( "#parent iframe" ).attr('src','https://www.youtube.com/embed/XRLAhcEPHlc?rel=0&amp;amp;controls=1;autoplay=1');
+                     $('.testimonial-text').text("It is better to travel well than to arrive-Jovita");
+
+                });
+
+                $( "#rodion_video" ).click(function() {
+
+                    $('#play_button_image').hide(); 
+                    $( "#parent iframe" ).attr('src','https://www.youtube.com/embed/yawXq5TlgBU?rel=0&amp;amp;controls=1;autoplay=1');
+                    $('.testimonial-text').text("It is better to travel well than to arrive-Rodion");
+                    
+
+                });
+                
+              
+                $( "# jovita_video_dummy" ).click(function() {
+
+                    $('#play_button_image').hide(); 
+                    $( "#parent iframe" ).attr('src','https://www.youtube.com/embed/yawXq5TlgBU?rel=0&amp;amp;controls=1;autoplay=1');
+                      $('.testimonial-text').text("It is better to travel well than to arrive-Jovita");
+                });
+                
+
+            });   
+        
+
